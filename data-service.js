ccfb6d5520807ab52bc98517d2fa1cf15e4a22d1
Philip Mroczkowski <pmroczkowski@myseneca.ca>
2018-10-03 19:41:00 -0400

    Updated server and data

---
+++ data-service.js
@@ -0,0 +1,128 @@
+var fs = require("fs");
+
+var employess = []; 
+var departments = []; 
+
+module.exports.initialize = function(){
+    return new Promise(function(resolve,reject){
+        try{
+            fs.readFile('./data/employees.json', function(err, data){
+                if(err) throw err;
+                employess = JSON.parse(data);
+            });
+            fs.readFile('./data/departments.json', function(err,data){
+                if(err) throw err;
+                departments = JSON.parse(data);
+            });
+        }catch(ex){
+            reject("Unable to read file!");
+        }
+        resolve("successfully read JSON file.");
+    });
+}
+
+module.exports.getAllEmployees = function(){
+    var arryAllEmployees=[];
+    return new Promise(function(resolve,reject){
+        for (var i = 0; i < employess.length; i++) {
+            arryAllEmployees.push(employess[i]);
+        }
+        if (arryAllEmployees.length == 0){
+            reject("No Result Returned");
+        }
+    resolve(arryAllEmployees);
+    })
+}
+
+module.exports.getEmployeesByStatus = function(status){
+    var arryByStatus = [];
+    return new Promise(function(resolve,reject){
+        for(let i = 0; i < employess.length; i++){
+            if(employess[i].status == status){
+                arryByStatus.push(employess[i]);
+            }
+        }
+        if (arryByStatus.length == 0){
+            reject("No Result Returned");
+        }
+        resolve(arryByStatus);
+    });
+}
+
+module.exports.getEmployeesByDepartment = function(department){
+    var arryByDepartment = [];
+    return new Promise(function(resolve,reject){
+        for(let i = 0; i < employess.length; i++){
+            if(employess[i].department == department){
+                arryByDepartment.push(employess[i]);
+            }
+        }
+        if(arryByDepartment.length == 0){
+            reject("No Result Returned");
+        }
+    resolve(arryByDepartment);
+    });
+}
+
+module.exports.getEmployeesByManager = function(manager) {
+    var arrayGetEmployeesByMannager = [];
+
+    return new Promise(function(resolve,reject) {
+        for (let i = 0; i < employess.length; i++) {
+            if (employess[i].employeeManagerNum == manager) {
+                arrayGetEmployeesByMannager.push(employess[i]);
+            }
+        }
+        if (arrayGetEmployeesByMannager.length == 0 ) {
+            reject("No Result Returned");
+        }
+    resolve(arrayGetEmployeesByMannager);
+    });
+}
+
+module.exports.getEmployeeByNum = function(num) {
+    return new Promise(function(resolve,reject){
+        for(let j = 0; j < employess.length; j++){
+            if(employess[j].employeeNum == num){
+                resolve(employess[j]);
+            }
+        }
+    reject("No Result Returned");
+    });
+}
+
+module.exports.getManagers = function() {
+    var arryGetManagers = [];
+    return new Promise(function(resolve,reject){
+        if(employess.length == 0){
+            reject("No Result Returned");
+        }else{
+            for (var q = 0; q < employess.length; q++) {
+                 if (employess[q].isManager == true) {
+                    arryGetManagers.push(employess[q]);       
+                 }
+            }
+            if (arryGetManagers.length == 0) {
+                     reject("No Result Returned");
+             }
+        }
+        resolve(arryGetManagers);
+     });
+}
+
+module.exports.getDepartments = function() {
+    var arryGetDepartments = [];
+    return new Promise(function(resolve,reject){
+        if(employess.length == 0){
+            reject("No Result Returned");
+        }else{
+            for (var v = 0; v < departments.length; v++) {
+                arryGetDepartments.push(departments[v]);       
+            }
+            if (arryGetDepartments.length == 0) {
+                reject("No Result Return");
+            }
+        }
+    resolve(arryGetDepartments);
+    });
+}
--- server.js
+++ server.js
@@ -1,23 +1,99 @@
+/*********************************************************************************
+*  WEB322 –Assignment02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Philip Mroczkowski Student ID: 21772174 Date: 2018-10-03
+*
+*  Online (Heroku) Link:  
+*
+********************************************************************************/ 
 var express = require("express");
 var app = express();
+var path = require("path");
+var data_service = require("./data-service.js");
 
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 3000;
 
-// call this function after the http server starts listening for requests
 function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
+    console.log("Express http server listening on: " + HTTP_PORT);
+    return new Promise (function(res,req){
+    data_service.initialize().then(function(data){
+      console.log(data)
+    }).catch(function(err){
+      console.log(err);
+    });
+});
 }
-app.use(express.static('public')); 
-// setup a 'route' to listen on the default url path (http://localhost)
+
+// Load CSS file
+app.use(express.static('public'));
+
 app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "\WEB\web322-app\views"));
-  });
 
-// setup another route to listen on /about
+   res.sendFile(path.join(__dirname + "/views/home.html"));
+});
+
 app.get("/about", function(req,res){
-    res.sendFile(path.join(__dirname, "\WEB\web322-app\views"));
+  res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+app.get("/employees", function(req,res){
+
+    if(req.query.status){
+      data_service.getEmployeesByStatus(req.query.status).then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+    }else if(req.query.department){
+      data_service.getEmployeesByDepartment(req.query.department).then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+    }else if(req.query.manager){
+      data_service.getEmployeesByManager(req.query.manager).then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+    }else{
+      data_service.getAllEmployees().then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+    }
+});
+
+app.get("/employee/:num", function(req,res){
+  data_service.getEmployeeByNum(req.params.num).then(function(data){
+    res.json(data);
+  }).catch(function(err){
+      res.json({message: err});
   });
+});
 
+app.get("/managers", function(req,res){
+      data_service.getManagers().then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+});
 
-// setup http server to listen on HTTP_PORT
+app.get("/departments", function(req,res){
+      data_service.getDepartments().then(function(data){
+        res.json(data);
+      }).catch(function(err){
+        res.json({message: err});
+      });
+});
+
+app.use(function(req, res) {
+  res.status(404).send("Sorry, Page Not Found! ");
+});
+
 app.listen(HTTP_PORT, onHttpStart);
