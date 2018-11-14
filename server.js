ae44419776d39fdfb8828b13f0c087d24ac98b52
Philip Mroczkowski <pmroczkowski@myseneca.ca>
2018-11-14 13:10:24 -0500

    a4

--- server.js
+++ server.js
@@ -14,81 +14,110 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var dataService = require("./data-service.js");
-var bodyParser = require('body-parser'); // new
-var multer = require("multer"); // new
-var path = require("path"); // new
-var fs = require("fs"); // new
+var bodyParser = require('body-parser');
+var multer = require("multer");
+var path = require("path");
+var fs = require("fs");
+const exphbs = require('express-handlebars'); // new
 
 var http_port = process.env.PORT || 8080;
 
 function onHttpStart() {
     console.log("Express http server listening on " + http_port);
 }
-
-
 // new
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: 'main',
+    helpers: {
+        navLink: (url, options) => {
+            return '<li' + 
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + 
+                '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        
+        equal: (lvalue, rvalue, options) => {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }
+    }
+}));
+app.set("view engine", ".hbs");
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
-//new
+
 const storage = multer.diskStorage({
     destination: "./public/images/uploaded",
     filename: function(req, file, cb){
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
-// new
+
 const upload = multer({storage: storage});
+//new
+app.use(function(req,res,next){
+    let route = req.baseUrl + req.path;
+    app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+    next();
+});
+
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.render("home"); // new
 });
 
 app.get("/about", function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+    res.render("about"); // new
 });
-// new
+
 app.get("/employees", function(req, res) {
     if(req.query.status){
         dataService.getEmployeesByStatus(req.query.status)
         .then(function(data){
-            res.json(data);
+            res.render("employees", {employees: data}); // new
         })
         .catch(function(err){
-            res.send(err);
+            res.render({message: "no results"}); // new
         })
     }
     else if(req.query.department){
         dataService.getEmployeesByDepartment(req.query.department)
         .then(function(data){
-            res.json(data);
+            res.render("employees", {employees: data}); // new
         })
         .catch(function(err){
-            res.send(err);
+            res.render({message: "no results"}); // new
         })
     }
     else if(req.query.manager){
         dataService.getEmployeesByManager(req.query.manager)
         .then(function(data){
-            res.json(data);
+            res.render("employees", {employees: data}); // new
         })
         .catch(function(err){
-            res.send(err);
+            res.render({message: "no results"}); // new
         })
     }
     else{
         dataService.getAllEmployees()
         .then(function(data){
-            res.json(data);
+            res.render("employees", {employees: data}); // new
         })
         .catch(function(err){
-            res.send(err);
+            res.render({message: "no results"}); // new
         })
     }    
 });
-//new
+
 app.get("/employees/value", function(req, res){
-    dataService.getEmployeesByNum(req.params.num)
+    dataService.getEmployeesByNum(req.body)
     .then(function(data){
         res.json(data);
     })
@@ -98,9 +127,9 @@ app.get("/employees/value", function(req, res){
 });
 
 app.get("/employees/add", function(req, res){
-    res.sendFile(path.join(__dirname + "/views/addEmployee.html"));
+    res.render("addEmployee");
 });
-// new
+
 app.post("/employees/add", function(req, res) {
     dataService.AddEmployee(req.body)
     .then(function(data){
@@ -110,38 +139,51 @@ app.post("/employees/add", function(req, res) {
         res.send(err);
     });
 });
-
-app.get("/managers", function(req, res) {
-    dataService.getManagers()
-    .then(function(data) {
-        res.json(data);
-    })
-    .catch(function(error) {
-        res.json({"message": error});
-    })
+// new
+app.get("/employee/:num", (req, res) => {
+    var num = req.params.num;
+    dataService.getEmployeesByNum(num).then((data) => {
+        res.render("employee", {
+            employee: data
+        });
+    }).catch((err) => {
+        res.render("employee", {
+            message: "no results"
+        });
+    });
 });
 
+// new
+app.post("/employee/update", function(req, res) {
+    dataService.updateEmployee(req.body)
+    .then(function() {
+      res.redirect("/employees");
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
 app.get("/departments", function(req, res) {
     dataService.getDepartments()
     .then(function(data) {
-        res.json(data);
+        res.render("departments", {departments: data});
     })
     .catch(function(error) {
         res.json({"message": error});
     })
 });
 
-// new
+
 app.get("/images", function(req, res){
     fs.readdir(__dirname + "/public/images/uploaded", function(err, images){
-         res.json({images});
+        res.render("images", {data: images}); //new
     });
 });
-//new
+
 app.get("/images/add", function(req, res){
-    res.sendFile(path.join(__dirname + "/views/addImage.html"));
+    res.render("addImage");
 });
-// new
+
 app.post("/images/add", upload.single("imageFile"), function(req, res) {
     res.redirect("/images");
 });
